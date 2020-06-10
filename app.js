e7c40a60318d393c65f300f9d74b18a959a77418
Hieu Pham <vanhieu1601@gmail.com>
2020-06-10 17:32:59 +0700

    added more functions

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 80;
 const app = express();
+app.use(bodyParser.json());
 
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebase/veggie-shop-firebase-adminsdk-vjdom-4f9603ad17.json");
@@ -10,17 +12,92 @@ admin.initializeApp({
 });
 const fs = admin.firestore();
 
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-  return res.json({ value: 'good'});
-})
+app.get("/", (req, res) => {
+  return res.json({ value: "good" });
+});
 
-app.get("/users", (req, res) => {
-  const ref = fs.collection('users').get().then((data)=> {
+app.get("/users", async (req, res) => {
+  const users = [];
+  const querySnapshot = await fs.collection("users").get();
+  querySnapshot.forEach((doc) =>
+    users.push({
+      id: doc.id,
+      ...doc.data(),
+    })
+  );
 
-  });
+  return res.json(users);
+});
+
+// grant admin permission to a user
+app.post("/admin", async (req, res) => {
+  const uid = req.body.uid;
+  console.log(uid);
+  try {
+    const ref = fs
+      .collection("roles")
+      .doc("admin")
+      .collection("role-users")
+      .doc(uid);
+
+    await ref.set({
+      active: true,
+    });
+
+    return res.send(true);
+  } catch {
+    return res.send(false);
+  }
+});
+
+// activate / deactive an admin
+app.put("/admin/:uid/:status", async (req, res) => {
+  const uid = req.params.uid;
+  const status = req.params.status;
+
+  try {
+    const ref = fs
+      .collection("roles")
+      .doc("admin")
+      .collection("role-users")
+      .doc(uid);
+
+    if (status == "true") {
+      await ref.update({
+        active: true,
+      });
+    } else if (status == "false") {
+      await ref.update({
+        active: false,
+      });
+    } else {
+      throw new Error("Invalid params");
+    }
+    return res.send(true);
+  } catch {
+    return res.send(false);
+  }
+});
+
+// delete an admin
+app.delete("/admin/:uid", async (req, res) => {
+  const uid = req.params.uid;
+
+  try {
+    const ref = fs
+      .collection("roles")
+      .doc("admin")
+      .collection("role-users")
+      .doc(uid);
+
+    await ref.delete();
+
+    return res.send(true);
+  } catch {
+    return res.send(false);
+  }
 });
