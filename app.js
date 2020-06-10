6c1e2873321bf2160f58ba822e6616e45373a1f5
Hieu Pham <vanhieu1601@gmail.com>
2020-06-10 15:44:24 +0700

    .gitignore fix

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const port = process.env.PORT || 80;
+const app = express();
 
 const admin = require("firebase-admin");
 const serviceAccount = require("./firebase/veggie-shop-firebase-adminsdk-vjdom-4f9603ad17.json");
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://veggie-shop.firebaseio.com",
 });
+const fs = admin.firestore();
 
-const app = express();
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
@@ -19,4 +19,8 @@ app.get('/', (req, res) => {
   return res.json({ value: 'good'});
 })
 
-app.get("/users", (req, res) => {});
+app.get("/users", (req, res) => {
+  const ref = fs.collection('users').get().then((data)=> {
+
+  });
+});
